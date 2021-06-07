753e964fe8456595c0eb083beb18c4fd29760ab1
zhangran19 <zhangran19@tal.com>
2021-06-07 15:59:34 +0800

    添加项目文件。

---
+++ CefSahrp_Test/Views/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CefSahrp_Test.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool isInkTop = false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isInkTop)
+            {
+                ink.EditingMode = System.Windows.Controls.InkCanvasEditingMode.None;
+                //btn.SetValue(Panel.ZIndexProperty, 2);
+                //cef.SetValue(Panel.ZIndexProperty, 1);
+                //ink.SetValue(Panel.ZIndexProperty, 0);
+                //ink.IsHitTestVisible = false;
+            }
+            else
+            {
+                ink.EditingMode = System.Windows.Controls.InkCanvasEditingMode.Ink;
+                //ink.IsHitTestVisible = false;
+                //btn.SetValue(Panel.ZIndexProperty, 2);
+                //ink.SetValue(Panel.ZIndexProperty, 1);
+                //cef.SetValue(Panel.ZIndexProperty, 0);
+                //ink.IsHitTestVisible = true;
+            }
+            isInkTop = !isInkTop;
+        }
+
+        private void cef_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            cef.Focus();
+        }
+    }
+}
