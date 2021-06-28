1788c9c1db6ce275576ba356f1950114e6a79cc3
zhangran19 <zhangran19@tal.com>
2021-06-28 17:41:32 +0800

    1

---
+++ CefS_JS_Test/Base/JSCallSCObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CefS_JS_Test.Base
+{
+    public class JSCallSCObject
+    {
+        public void Functions(string type,string jsonObject,string param=null)
+        {
+            switch (type.Trim())
+            {
+                case "FormulaInsert":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
