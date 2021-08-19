ae7ab94bdfc737c8221564c34039d9c6d829c310
zhangran19 <zhangran19@tal.com>
2021-08-19 17:38:20 +0800

    11

---
+++ CEF_CPU_TEST/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CEF_CPU_TEST.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace CEF_CPU_TEST
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ CEF_CPU_TEST/Views/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CEF_CPU_TEST.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //CefSettings settings = new CefSettings();
+            //settings.CefCommandLineArgs.Add("disable-gpu", "1");
+            ////settings.SetOffScreenRenderingBestPerformanceArgs();
+            //Cef.Initialize(settings);
+            InitializeComponent();
+            //foreach (var item in wrap.Children)
+            //{
+            //    Grid grid = item as Grid;
+            //    ChromiumWebBrowser chromiumWebBrowser = grid.Children[0] as ChromiumWebBrowser;
+            //    var browserSettings = new CefSharp.BrowserSettings();
+            //    browserSettings.WindowlessFrameRate = 1;
+            //    chromiumWebBrowser.BrowserSettings = browserSettings;
+            //}
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in wrap.Children)
+            {
+                Grid grid = item as Grid;
+                ChromiumWebBrowser chromiumWebBrowser = grid.Children[0] as ChromiumWebBrowser;
+                chromiumWebBrowser.BrowserSettings = new BrowserSettings();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in wrap.Children)
+            {
+                Grid grid = item as Grid;
+                ChromiumWebBrowser chromiumWebBrowser = new ChromiumWebBrowser();
+                var browserSettings = new CefSharp.BrowserSettings();
+                browserSettings.WindowlessFrameRate = 30;
+                chromiumWebBrowser.BrowserSettings = browserSettings;
+                chromiumWebBrowser.Address = "https://mv.xesimg.com/XESlides/jssdk/1.5.7/preview/index.html?fileJson=https://mv.xesimg.com/XESlides/slidev2/slide_175914/1622624173906.json";
+            grid.Children.Clear();
+                grid.Children.Add(chromiumWebBrowser);
+            }
+        }
+    }
+}
