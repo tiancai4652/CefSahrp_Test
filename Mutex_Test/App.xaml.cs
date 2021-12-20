efec050e70a23f1a8ec8eee9ec15d9f0f370ce89
zr <zhangran19@tal.com>
2021-12-20 16:37:58 +0800

    11

---
+++ Mutex_Test/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Mutex_Test.Views;
+using Prism.Ioc;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace Mutex_Test
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            bool createdNew = false
+                ;
+            using (Mutex mutex = new Mutex(true, "TalWhiteBoard", out createdNew))
+            {
+                if (createdNew)
+                {
+                    return Container.Resolve<MainWindow>();
+                }
+                else
+                {
+                    MessageBox.Show("当前只允许运行一个白板程序!");
+                    Environment.Exit(-2);
+                    return null;
+                }
+            }
+          
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ ScrollViewer_Touch_Test/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using ScrollViewer_Touch_Test.Views;
+using System.Windows;
+
+namespace ScrollViewer_Touch_Test
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
