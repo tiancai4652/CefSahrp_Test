7f991913e64fd7dad5880f52c9bbcfff3721ca61
zr <zhangran19@tal.com>
2022-02-07 16:34:18 +0800

    11

---
+++ Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            //Client
+            var client = new NamedPipeClientStream("PipesOfPiece");
+            client.Connect();
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input)) break;
+                writer.WriteLine(input);
+                writer.Flush();
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+
+   
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+            Task.Delay(1000).Wait();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static void StartServer()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var server = new NamedPipeServerStream("PipesOfPiece");
+                server.WaitForConnection();
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    writer.WriteLine(String.Join("", line.Reverse()));
+                    writer.Flush();
+                }
+            });
+        }
+    }
+}
