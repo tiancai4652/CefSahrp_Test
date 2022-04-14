7963bd03f5b22430c7738786152ff0da0696e8e3
zr <zhangran19@tal.com>
2022-04-14 18:28:06 +0800

    1

--- ConsoleApp1/CefGetScreenShot.cs
+++ ConsoleApp1/CefGetScreenShot.cs
@@ -18,22 +18,21 @@ namespace ConsoleApp1
 {
     public class CefGetScreenShot
     {
-        public CefGetScreenShot(string url,  Action<string> action = null)
+        public CefGetScreenShot(string url,  Action<List<ImageSource>> action = null)
         {
             Url = url;
             GetImageAction = action;
         }
 
-        public Action<string> GetImageAction { get; set; }
+        public Action<List<ImageSource>> GetImageAction { get; set; }
+        public string Url { get; set; }
+        public ChromiumWebBrowser Browser { get; set; }
+        public int LoadingTimeMs { get; set; } = 500;
+        public bool IsSaveImage { get; set; } = false;
 
         public void Start()
         {
-
-            //要截取图片的网页URL
-            string testUrl = Url;
-
-         
-            Browser = new ChromiumWebBrowser(testUrl);
+            Browser = new ChromiumWebBrowser(Url);
             Browser.FrameLoadStart += (s, argsi) =>
             {
                 var b = (ChromiumWebBrowser)s;
@@ -47,9 +46,7 @@ namespace ConsoleApp1
             Console.WriteLine("加载完成 " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
         }
 
-        public string Url { get; set; }
-        public ChromiumWebBrowser Browser { get; set; }
-
+        
         void InitJS()
         {
             TestClass t = new TestClass();
@@ -74,107 +71,50 @@ namespace ConsoleApp1
             {
                 if (Browser.CanExecuteJavascriptInMainFrame)
                 {
-                    //网页截图保存地址
                     string imgName = "CefSharp_screenshot" + DateTime.Now.Ticks + ".jpg";
-                    //imgName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), imgName);
                     try
                     {
                         getThumbnailClass getThumbnailClass = JsonConvert.DeserializeObject<getThumbnailClass>(msg);
+
+                        Browser.Size = new System.Drawing.Size(getThumbnailClass.width, getThumbnailClass.height * getThumbnailClass.thumbLen);
+
                         var cefbrowserHost = Browser.GetBrowserHost();
 
-                        //You can call Invalidate to redraw/refresh the image
                         cefbrowserHost.Invalidate(PaintElementType.View);
-
-                        ////获取内容尺寸
-                        //CefSharp.DevTools.DOM.Rect contentSize = null;
-                        //using (var devToolsClient = Browser.GetDevToolsClient())
-                        //{
-                        //    //Get the content size
-                        //    var layoutMetricsResponse = await devToolsClient.Page.GetLayoutMetricsAsync().ConfigureAwait(continueOnCapturedContext: false);
-
-                        //    contentSize = layoutMetricsResponse.ContentSize;
-                        //}
-
-                        //var viewport = new Viewport
-                        //{
-                        //    Height = getThumbnailClass.height * getThumbnailClass.thumbLen,
-                        //    Width = getThumbnailClass.width,
-                        //    Scale = 1
-                        //};
 
                         Console.WriteLine("截图...");
 
                         await Task.Factory.StartNew(async () =>
                         {
-                            await Task.Delay(500);
+                            await Task.Delay(LoadingTimeMs);
                             Bitmap bitmap = await Browser.ScreenshotAsync(true);
-                            bitmap.Save(imgName);
-
+                          
                             SplitImage splitImage = new SplitImage(getThumbnailClass.thumbLen, getThumbnailClass.width, getThumbnailClass.height);
-                         
 
                             MemoryStream ms = new MemoryStream();
                             bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-                            byte[] buffer = ms.GetBuffer();  //byte[]   bytes=   ms.ToArray(); 这两句都可以，至于区别么，下面有解释
+                            byte[] buffer = ms.GetBuffer();  
                             ms.Close();
 
-
-                            var list = splitImage.GetImages(buffer);
-
-                            int i = 0;
-                            list.ForEach(t =>
+                            if (GetImageAction != null)
                             {
-                                var name = i++.ToString() + ".png";
-                                t.Save(name);
-                            });
-
-
-                            Process.Start(imgName);
+                                GetImageAction.Invoke(splitImage.GetImageSources(buffer));
+                            }
+                            if (IsSaveImage)
+                            {
+                                var list = splitImage.GetImages(buffer);
+                                int i = 0;
+                                list.ForEach(t =>
+                                {
+                                    var name = i++.ToString() + ".png";
+                                    t.Save(name);
+                                });
+                                bitmap.Save(imgName);
+                                Process.Start(imgName);
+                            }
                         });
-                        //完整网页截图
-                        //var buffer = await browser.CaptureScreenshotAsync(viewport: viewport);
 
-
-                        //Class2 class2 = new Class2(Browser);
-                        //var buffer = await class2.CaptureScreenshotAsync(viewport: viewport);
-
-                        //MemoryStream ms = new MemoryStream();
-                        //bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-                        //byte[] buffer = ms.GetBuffer();  //byte[]   bytes=   ms.ToArray(); 这两句都可以，至于区别么，下面有解释
-                        //ms.Close();
-
-
-                        //System.IO.File.WriteAllBytes(imgName, buffer);
-                        //Console.WriteLine("截图保存完成");
-
-
-                        if (GetImageAction != null)
-                        {
-                            GetImageAction.Invoke(imgName);
-                        }
-
-
-                        //SplitImage splitImage = new SplitImage(getThumbnailClass.thumbLen, getThumbnailClass.width, getThumbnailClass.height);
-                        //var list= splitImage.GetBitmapSources(buffer);
-
-                        //int i = 0;
-                        //list.ForEach(t =>
-                        //{
-                        //    var name = i++.ToString()+".png";
-                        //    SaveImageToFile(t,name);
-                        //});
-
-                        //var list = splitImage.GetImages(buffer);
-
-                        //int i = 0;
-                        //list.ForEach(t =>
-                        //{
-                        //    var name = i++.ToString() + ".png";
-                        //    t.Save(name);
-                        //});
-
-
-
+                      
 
                     }
                     catch (Exception ex)
@@ -208,10 +148,28 @@ namespace ConsoleApp1
             if (Browser.CanExecuteJavascriptInMainFrame)
             {
                 //var task = await Browser.EvaluateScriptAsPromiseAsync("getThumbnail()" 
-                Browser.ExecuteScriptAsync("getThumbnail()");
+                ValidAndExcute("getThumbnail");
             }
+        }
 
+        async void ValidAndExcute(string funcName, string param = "")
+        {
+            if (Browser.CanExecuteJavascriptInMainFrame)
+            {
+                var task = await Browser.EvaluateScriptAsync("" +
+                "(" +
+                "function()" +
+                " { " +
+                $"return  typeof {funcName} != 'undefined';" +
+                " }" +
+                ")();");
 
+                var b = (bool)task.Result;
+                if (b)
+                {
+                    Browser.ExecuteScriptAsync($"{funcName}({param})");
+                }
+            }
         }
     }
 
