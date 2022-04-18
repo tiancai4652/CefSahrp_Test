40868c119ceccb2ac3fd199504f4296b6194adfe
zr <zhangran19@tal.com>
2022-04-18 14:40:19 +0800

    1

---
+++ WPFCef/Views/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using CefSharp;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WPFCef.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            browser.Address = @"http://10.14.144.210:8005/?fileJson=https://static0-test.xesimg.com/xeslidev3/course/146836671411781962/pro/146836671411781962_1649210491665.json&env=6";
+            image.Source = new BitmapImage(
+        new Uri("http://kjdsfz-cdn.jiaoyanyun.com/kejian-jietu/09d6776c89f94b4dbc1fa361e7a9790f/1-1614913149828-5fa4163522ae40e09511eea027a2dfb5.jpg"));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            browser.ShowDevTools();
+        }
+    }
+}
---
+++ Winform_cef/Form1.cs
@@ -0,0 +1,43 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_cef
+{
+    public partial class Form1 : Form
+    {
+        ChromiumWebBrowser chromeBrowser;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string page =  @"http://10.14.144.210:8005/?fileJson=https://static0-test.xesimg.com/xeslidev3/course/146836671411781962/pro/146836671411781962_1649210491665.json&env=6";
+
+            var settings = new CefSettings();
+            settings.CefCommandLineArgs.Add("disable-gpu"); // Disable GPU acceleration
+            settings.CefCommandLineArgs.Add("disable-gpu-vsync"); //Disable GPU vsync
+
+            Cef.Initialize(settings);
+
+            chromeBrowser = new ChromiumWebBrowser(page);
+
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chromeBrowser.ShowDevTools();
+        }
+    }
+}
