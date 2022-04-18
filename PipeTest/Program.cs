d5b325c74174017629908a97aa5d8449136e42d7
zr <zhangran19@tal.com>
2022-04-18 15:15:40 +0800

    1

---
+++ PipeTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+
+namespace PipeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "PipesOfTalWhiteboard";
+
+            NamedPipeServerStream NamedPipeServerStream = new NamedPipeServerStream("PipesOfTalWhiteboard");
+
+            String[] listOfPipes = System.IO.Directory.GetFiles(@"\\.\pipe\");
+
+            var x = listOfPipes.Contains(name);
+
+            var xx = listOfPipes.Where(t => { return t.Contains(name); });
+
+            NamedPipeServerStream.WaitForConnection();
+            Console.ReadKey();
+        }
+    }
+}
